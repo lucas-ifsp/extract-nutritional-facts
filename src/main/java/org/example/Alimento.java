5b82abe3b1b18afaf0cf83bf2a0ca076a95567f7
prof-lucas-ifsp <lucas.oliveira@ifsp.edu.br>
2022-12-02 22:45:46 -0300

    solves the issue

--- src/main/java/org/example/Alimento.java
+++ src/main/java/org/example/Alimento.java
@@ -1,2 +1,5 @@
-package org.example;public record Alimento() {
+package org.example;
+
+public record Alimento(Integer id, String nome, Integer porcao, Integer calorias, Integer colesterol, Double proteinas,
+                       Boolean gluten, Double gordurasSaturadas, Double sodio, Double acucar, Double lactose) {
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,68 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        var path = Paths.get("src/main/resources/nutrition.csv");
+        try (var reader = Files.newBufferedReader(path)) {
+            reader.lines()
+                    .skip(1) //cabeçalho
+                    .map(Main::removeQuotesBetweenQuotes)
+                    .map(Main::replaceCommaBySemicolon)
+                    .map(Main::replaceSemicolonByCommaBetweenQuotes)
+                    .map(Main::removeQuotes)
+                    .map(Main::toAlimento)
+                    .forEach(System.out::println);
+        }
+    }
+
+    private static String removeQuotesBetweenQuotes(String line) {
+        return line.replace("\"\"", "");
+    }
+
+    private static String replaceCommaBySemicolon(String line) {
+        return line.replace(",", ";");
+    }
+
+    private static String replaceSemicolonByCommaBetweenQuotes(String line){
+        final Pattern textBetweenQuotesPattern = Pattern.compile("\"(.*)\"");
+        final Matcher matcher = textBetweenQuotesPattern.matcher(line);
+        if(!matcher.find()) return line;
+        final String textBetweenQuotes = matcher.group();
+        return line.replace(textBetweenQuotes, textBetweenQuotes.replace(";", ","));
+    }
+
+    private static String removeQuotes(String line) {
+        return line.replace("\"", "");
+    }
+
+    private static Alimento toAlimento(String line){
+        final String[] data = line.split(";");
+        Integer id = Integer.parseInt(data[0]);
+        String nome = data[1];
+        Integer porcao = Integer.parseInt(extractNumberPart(data[2]));
+        Integer calorias = Integer.valueOf(data[3]);
+        Double gordurasSaturadas = Double.valueOf(extractNumberPart(data[5]));
+        Integer colesterol = Integer.valueOf(extractNumberPart(data[6]));
+        Double proteina = Double.valueOf(extractNumberPart(data[38]));
+        Double sodio = Double.valueOf(extractNumberPart(data[7]));
+        Double acucar = Double.valueOf(extractNumberPart(data[60]));
+        Double lactose = Double.valueOf(extractNumberPart(data[64]));
+        return new Alimento(id,nome,porcao,calorias, colesterol,
+                proteina, false, gordurasSaturadas, sodio, acucar, lactose);
+    }
+
+    private static String extractNumberPart(String text){
+        if(text == null || text.isEmpty()) return "0";
+        Pattern numberPattern = Pattern.compile("\\d+\\.?\\d*");
+        Matcher matcher = numberPattern.matcher(text);
+        if (matcher.find()) return matcher.group();
+        return text;
     }
 }
